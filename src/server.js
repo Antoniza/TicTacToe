3a8a52f767477c7e18e010ab3ffb89a2cdaf7c75
Antoniza <antoniza.developer@gmail.com>
2023-04-07 00:29:39 -0600

    Somes websocket settings

--- src/server.js
+++ src/server.js
@@ -16,7 +16,7 @@ app.use(express.static(path.join(__dirname, "./public")));
 
 const webServer = new WebSocket({
   httpServer: server,
-  autoAcceptConnections: true,
+  autoAcceptConnections: false,
 });
 
 let usersConected = [];
