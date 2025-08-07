47bbd4b8ab3c8c724b7f08096bd146f141b8b7d5
Antoniza <antoniza.developer@gmail.com>
2025-08-07 09:07:14 -0600

    UPGRADE 2025-8

--- src/public/js/webSocketController.js
+++ src/public/js/webSocketController.js
@@ -321,7 +321,17 @@ function wsConnect() {
   });
 }
 
-function onOpen(evt) {}
+function onOpen(evt) {
+  console.log("Conectado al servidor WebSocket");
+  document.getElementById("login-form").addEventListener("submit", login, false);
+  document.getElementById("message-form").addEventListener("submit", sendData, false);
+  document.querySelectorAll(".imgBoard").forEach((element) => {
+    element.addEventListener("click", fillSpace, false);
+  });
+  document.getElementById("alert-button").addEventListener("click", () => {
+    document.getElementById("alert").style.display = "none";
+  });
+}
 
 function onClose(evt) {
   alert("Te has deconectado del servidor");
